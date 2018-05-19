568afee010078d9cdbdd0ca85a567e608acf8e2b
Sercan Yildiz <ysercanz@gmail.com>
2018-05-19 23:20:36 +0200

    add keyboard control

--- src/App.js
+++ src/App.js
@@ -54,14 +54,83 @@ const LevelOne = ({playerPosition, onClick}) => {
 class App extends Component {
   state = {
     playerPosition: {
-      x: 0,
-      y: 0
+      x: 0, // top bottom
+      y: 2 // lleft right
     }
   }
 
   constructor (props) {
     super(props)
     this.onClick = this.onClick.bind(this)
+    this.keysHandkler = this.keysHandkler.bind(this)
+    this.movePlayer = this.movePlayer.bind(this)
+  }
+
+  keysHandkler (e) {
+    console.log('e', e.code)
+    switch (e.code) {
+      case 'ArrowUp':
+        this.movePlayer('ArrowUp')
+        break;
+      case 'ArrowDown':
+        this.movePlayer('ArrowDown')
+        break;
+      case 'ArrowLeft':
+        this.movePlayer('ArrowLeft')
+        break;
+      case 'ArrowRight':
+        this.movePlayer('ArrowRight')
+        break;
+    }
+  }
+
+  movePlayer (dir) {
+    console.log('dir', dir)
+    let x = this.state.playerPosition.x
+    let y = this.state.playerPosition.y
+    const minX = 0
+    const maxX = 9
+    const minY = 0
+    const maxY = 9
+
+    switch (dir) {
+      case 'ArrowUp':
+        if (x > minX) {
+          x = x - 1
+        }
+        break;
+      case 'ArrowDown':
+        if (x < maxX) {
+          x = x + 1
+        }
+        break;
+      case 'ArrowLeft':
+        if (y > minY) {
+          y = y - 1
+        }
+        break;
+      case 'ArrowRight':
+        if (y < maxY) {
+          y = y + 1
+        }
+        break;
+    }
+
+    this.setState(prevState => ({
+      playerPosition: {
+        x: x,
+        y: y
+      }
+    }))
+  }
+
+  componentWillMount () {
+    document.addEventListener('keydown', this.keysHandkler.bind(this))
+  }
+
+
+  componentWillUnmount () {
+    document.removeEventListener('keydown', this.keysHandkler.bind(this))
   }
 
   onClick (e) {
